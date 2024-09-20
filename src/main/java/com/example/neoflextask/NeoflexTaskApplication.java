4b484a2700659f5b0e7b3788de9de5b93c2910a4
iljaKorn <kornilov.ilja@rambler.ru>
2024-09-20 16:53:44 +0300

    init

---
+++ src/main/java/com/example/neoflextask/NeoflexTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.neoflextask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeoflexTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeoflexTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/neoflextask/NeoflexTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.neoflextask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeoflexTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
