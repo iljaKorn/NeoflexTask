3abc022c9aae56727b55052a12e4ced933b9ec3e
iljaKorn <kornilov.ilja@rambler.ru>
2024-09-22 17:56:59 +0300

    overloaded calculateVacationPay method (additional parameter "startDate") in VacationPayController

--- src/main/java/com/example/neoflextask/controller/VacationPayController.java
+++ src/main/java/com/example/neoflextask/controller/VacationPayController.java
@@ -1,19 +1,30 @@
 package com.example.neoflextask.controller;
 
 import com.example.neoflextask.service.VacationPayService;
-import org.springframework.stereotype.Controller;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.time.LocalDate;
+
+@RestController
+//@CrossOrigin(origins = "http://localhost:3000")
 public class VacationPayController {
     private final VacationPayService vacationPayService;
 
-    public VacationPayController(VacationPayService vacationPayService){
+    public VacationPayController(VacationPayService vacationPayService) {
         this.vacationPayService = vacationPayService;
     }
-    @GetMapping("/calculacte")
-    public Double calculateVacationPay(@RequestParam double avgSalary, @RequestParam int numOfDays){
+
+    @GetMapping(value = "/calculacte", params = {"avgSalary", "numOfDays"})
+    public Double calculateVacationPay(@RequestParam double avgSalary, @RequestParam int numOfDays) {
         return vacationPayService.calculateVacationPay(avgSalary, numOfDays);
     }
+
+    @GetMapping(value = "/calculacte", params = {"avgSalary", "numOfDays", "startDate"})
+    public Double calculateVacationPay(@RequestParam double avgSalary, @RequestParam int numOfDays, @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate startDate) {
+        return vacationPayService.calculateVacationPayFromDate(avgSalary, numOfDays, startDate);
+    }
+
 }
