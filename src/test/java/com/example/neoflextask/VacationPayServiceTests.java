a8c97048b708d7f5932a6606670f973d86d264c0
iljaKorn <kornilov.ilja@rambler.ru>
2024-09-22 17:58:07 +0300

    add VacationPayServiceTests

--- src/test/java/com/example/neoflextask/VacationPayServiceTests.java
+++ src/test/java/com/example/neoflextask/VacationPayServiceTests.java
@@ -3,6 +3,8 @@ package com.example.neoflextask;
 import com.example.neoflextask.service.VacationPayService;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class VacationPayServiceTests {
@@ -10,8 +12,25 @@ public class VacationPayServiceTests {
     @Test
     public void calculateVacationPay(){
         VacationPayService vacationPayService = new VacationPayService();
-        double averageSalary = 20000;
-        int numberDays = 12;
-        assertEquals(vacationPayService.calculateVacationPay(averageSalary, numberDays), 8191);
+        double avgSalary = 20000;
+        int numOfDays = 12;
+        assertEquals(vacationPayService.calculateVacationPay(avgSalary, numOfDays), 8191);
+    }
+
+    @Test
+    public void calculateVacationPayFromDateWithoutHolidays(){
+        VacationPayService vacationPayService = new VacationPayService();
+        double avgSalary = 20000;
+        int numOfDays = 12;
+        LocalDate date = LocalDate.of(2022, 8, 10);
+        assertEquals(vacationPayService.calculateVacationPayFromDate(avgSalary, numOfDays, date), 8191);
+    }
+    @Test
+    public void calculateVacationPayFromDateWithHolidays(){
+        VacationPayService vacationPayService = new VacationPayService();
+        double avgSalary = 20000;
+        int numOfDays = 12;
+        LocalDate date = LocalDate.of(2022, 5, 1);
+        assertEquals(vacationPayService.calculateVacationPayFromDate(avgSalary, numOfDays, date), 6826);
     }
 }
